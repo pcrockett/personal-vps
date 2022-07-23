d77cb35de47a8e150d60552a5836c5849eaa5e68
Philip Crockett <contact@philcrockett.com>
2022-07-23 22:51:05 +0200

    Should have gone with prev commit

--- targets/password-changed.sh
+++ targets/password-changed.sh
@@ -10,5 +10,7 @@ reached_if() {
 apply() {
     # We don't actually need to know the password; we have passwordless sudo.
     # I just don't like that OVH set my password for me.
+    # Note that since `echo` is a Bash builtin, this shouldn't cause a password leak.
     echo "${USER}:$(secure_random_hex 32)" | sudo chpasswd
+    touch "${STATE_DIR}/password-changed"
 }
