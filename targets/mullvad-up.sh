a2f22f53fd8e8120dad761922ddae8eb7f149f27
Philip Crockett <contact@philcrockett.com>
2023-01-05 10:33:47 +0100

    Don't permanently lock yourself out of your server...

--- targets/mullvad-up.sh
+++ targets/mullvad-up.sh
@@ -15,5 +15,5 @@ apply() {
     # We don't want Mullvad's DNS server:
     sudo sed "s/^DNS.*\$//" "${random_config}" | sudo tee /etc/wireguard/wg0.conf > /dev/null
 
-    sudo systemctl enable --now wg-quick@wg0.service
+    sudo systemctl start wg-quick@wg0.service
 }
