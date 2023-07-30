197ff92df3b5801b2793681d99d889b46347a5e2
Philip Crockett <contact@philcrockett.com>
2023-07-30 21:56:58 +0200

    should be using the DOCKER-USER chain when firewalling containers

--- config/firewall/firewall.sh
+++ config/firewall/firewall.sh
@@ -23,24 +23,38 @@ allow_public() {
     iptables --append INPUT --protocol "${protocol}" --dport "${port}" --jump ACCEPT
 }
 
+allow_public_docker() {
+    local port="${1}"
+    local protocol="${2:-tcp}"
+    iptables --append DOCKER-USER --protocol "${protocol}" --dport "${port}" --jump ACCEPT
+}
+
+allow_tailscale_docker() {
+    local port="${1}"
+    local protocol="${2:-tcp}"
+    iptables --append DOCKER-USER --protocol "${protocol}" --dport "${port}" --in-interface tailscale0 --jump ACCEPT
+}
+
 # SSH
 allow_tailscale 22 tcp
 
 # Nextcloud
 # https://github.com/nextcloud/all-in-one#explanation-of-used-ports
-allow_public 80 tcp       # allows retrieval of ACME TLS certificates, redirects to https
-allow_public 443 tcp      # main nextcloud interface
-# allow_public 443 udp      # main nextcloud interface (http3)
-# allow_public 3478 tcp     # TURN -- not needed, Nextcloud Talk is disabled for this low-end server
-# allow_public 3478 udp     # TURN -- not needed, Nextcloud Talk is disabled for this low-end server
-allow_tailscale 8080 tcp  # master container interface
-allow_tailscale 8443 tcp  # master container interface with valid cert
+iptables --new-chain DOCKER-USER || true  # if docker isn't running, this makes sure the user chain at least exists
+allow_public_docker 80 tcp          # allows retrieval of ACME TLS certificates, redirects to https
+allow_public_docker 443 tcp         # main nextcloud interface
+allow_tailscale_docker 443 udp      # main nextcloud interface (http3)
+allow_tailscale_docker 3478 tcp     # TURN -- not needed, Nextcloud Talk is disabled for this low-end server
+allow_tailscale_docker 3478 udp     # TURN -- not needed, Nextcloud Talk is disabled for this low-end server
+allow_tailscale_docker 8080 tcp     # master container interface
+allow_tailscale_docker 8443 tcp     # master container interface with valid cert
 
 # This makes Tailscale direct connections possible: https://tailscale.com/kb/1082/firewall-ports/
 allow_public 41641 udp
 
 iptables --append INPUT --jump REJECT
 ip6tables --append INPUT --jump REJECT
+iptables --append DOCKER-USER --jump REJECT  # TODO: this breaks tailscale forwarding, which i use
 iptables --policy INPUT DROP
 ip6tables --policy INPUT DROP
 iptables --policy FORWARD DROP
